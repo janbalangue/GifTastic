0ff14f5c4d3176c074f6f636b0d10fc56aa07f67
Jan Balangue <jantonio.balangue@gmail.com>
2018-03-07 16:08:52 -0800

    Modified style.css, app.js, and index.html. Gifs now transitioning properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var queryURL;
 var newGifDiv = [];
 // var newGif;
 // var newGifRating;
-// var gifId = 0;
+var gifId = 0;
 
 
 function drawButtons() {
@@ -32,10 +32,10 @@ $("button").on("click", function (e) {
     $.ajax({ url: queryURL, method: "GET" }).done(function (response) {
         for (var i = 0; i < 10; i++) {
             if ((response.data[i].rating === "g" || response.data[i].rating === "pg") && response.data[i].images.fixed_height.width < 500) {
-                newGifDiv= $("<div>");
+                newGifDiv = $("<div>");
                 newGifDiv.addClass("float-left");
-                newGifDiv.append("Rating: " + response.data[i].rating + "<br>");
-                newGifDiv.append('<img class="gif float-left" src="' + response.data[i].images.fixed_height_still.url + '" data-state="still" data-still="' + response.data[i].images.fixed_height_still.url + '" data-animate="' + response.data[i].images.fixed_height.url + '">');
+                newGifDiv.append("<p>Rating: " + response.data[i].rating + "</p>");
+                newGifDiv.append('<img class="float-left gif" id=' + gifId + ' data-state="still" src="' + response.data[i].images.fixed_height_still.url + '" data-still="' + response.data[i].images.fixed_height_still.url + '" data-animate="' + response.data[i].images.fixed_height.url + '">');
                 console.log(response.data[i].images.fixed_height.url);
                 console.log(response.data[i].images.fixed_height_still.url);
                 // newGifRating = $("<figcaption>Rating: " + response.data[i].rating + "</figcaption>");
@@ -43,26 +43,28 @@ $("button").on("click", function (e) {
                 // newGif.append(newGifRating);
                 // newGifDiv[gifId].append(newGif);
                 $("#gifs").prepend(newGifDiv);
-                // gifId++;
+                gifId++;
             }
         }
     });
 });
 
 // creates new instrument button when clicking search button
-$("#search").on("click", function (event) {
-    event.preventDefault();
+$(".form-inline").on("click", "#search", function () {
     newInstrument = $("#instrument-search").val().trim();
-    topics.push(newInstrument);
-    drawButtons();
+    if (newInstrument !== undefined) {
+        topics.push(newInstrument);
+        drawButtons();
+    }
 
 });
 
 // // click image to toggle between gif and still image
-$("img").on("click", function (e) {
-    e.preventDefault();
-    console.log(this);
-    if ($(this).attr("data-state") == "still") {
+$("#gifs").on("click", ".gif", function () {
+    var state = $(this).attr("data-state");
+    var gifId = $(this).attr("id");
+    console.log(state);
+    if (state === "still") {
         var animate = $(this).attr("data-animate");
         $(this).attr("src", animate);
         $(this).attr("data-state", "animate");
